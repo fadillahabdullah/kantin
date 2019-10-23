1f5663df736f8eaef9ed86263ab5e472f77ededd
fadillahabdullah <fadil.abdullah97pps@gmail.com>
2019-10-23 08:12:47 +0700

    first commit

---
+++ js/routes.js
@@ -0,0 +1,27 @@
+var routes = [
+  // Index page
+  {
+    path: '/',
+    url: './index.html',
+    name: 'home',
+  },
+  // About page
+  {
+    path: '/about/',
+    url: './pages/about.html',
+    name: 'about',
+  },
+  {
+    path: '/barang/',
+    componentUrl: 'pages/barang.html',
+  },
+  {
+    path: '/editbarang/',
+    componentUrl: 'pages/edit_barang.html',
+  },
+
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
